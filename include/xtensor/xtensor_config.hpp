326891b35294c4d2511e5ef048545fc7da11000f
Laramie Leavitt <laramie.leavitt@gmail.com>
2019-03-25 13:50:54 -0700

    ... Actually throw the exception in XTENSOR_THROW.

--- include/xtensor/xtensor_config.hpp
+++ include/xtensor/xtensor_config.hpp
@@ -84,7 +84,7 @@
 
 #if (defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND)) && !defined(XTENSOR_NO_EXCEPTIONS)
 // Exceptions are enabled.
-#define XTENSOR_THROW(exception) do {} while (false)
+#define XTENSOR_THROW(exception) throw exception
 #else
 // Exceptions are disabled.
 #define XTENSOR_NO_EXCEPTIONS
